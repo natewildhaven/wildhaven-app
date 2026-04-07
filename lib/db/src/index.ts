349ac8b71fee1c4d1b431ccb18f80845e999538d
natewildhaven <nate.wildhaventutoring@gmail.com>
2026-04-07 15:42:26 +0100

    Update index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -14,3 +14,4 @@ export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
 
 export * from "./schema/index.js";
+export { eq, sql, and } from "drizzle-orm";
